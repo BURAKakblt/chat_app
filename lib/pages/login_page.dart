033005bdbc12f9c7a15a734936a1e8ca464ce8ca
BURAK <154697965+BURAKakblt@users.noreply.github.com>
2025-12-17 23:25:42 +0300

    feat: implement light mode theme and refactor login UI components

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget{
@@ -7,7 +8,51 @@ class LoginPage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            //logo
+              Icon(
+                  Icons.message,
+                      size: 60,
+                color:Theme.of(context).colorScheme.primary,
+              ),
 
+            const SizedBox(height: 50),
+
+
+            //Welcome back message
+         Text(
+           "Welcome back, you've been missed!",
+           style: TextStyle(
+             color: Theme.of(context).colorScheme.primary,
+             fontSize: 16,
+           ),
+         ),
+
+            const SizedBox(height: 25),
+
+            // email textfield
+             MyTextField(
+               hintText: "Email",
+               obscureText: false,
+             ),
+
+
+            const SizedBox(height: 10,),
+
+            // pw textfield
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+              ),
+            // login button
+
+            // register now
+          ]
+        ),
+      )
     );
 
   }
